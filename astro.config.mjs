25cf2c5c7dfddc51e02d60a3dcf9b840e15abeb3
Lakshay Mittal <lakshaymittal225@gmail.com>
2023-05-29 15:18:10 +0530

    robots: Fix location of the file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://abhiyanta.vercel.app",
   output: "server",
   adapter: vercel(),
   integrations: [tailwind(), sitemap()],
